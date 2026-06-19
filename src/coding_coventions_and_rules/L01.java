0c44deb9920cb9538da31d21b345f796fb7daf2d
Rahul Goyal <rahulgoyal83789@gmail.com>
2026-06-19 12:30:46 +0530

    comments and conventions done

---
+++ src/coding_coventions_and_rules/L01.java
@@ -0,0 +1,25 @@
+package coding_coventions_and_rules;
+
+public class L01 {
+    static void main(String[] args) {
+        // these are allowed
+        int age;
+        int _age; // start with underscore
+        int _age123; // can have numbers
+        int $age123; // start with dollar symbol
+        //these are not allowed
+//        int 1age; // cant start with number
+//        int \age; // special character
+//        int age'; // special character
+//        int age n; // spaces are not allowed
+//        int int; // reserved keyword
+//        a and A are different
+
+
+//        Naming Cases
+//        CamelCase for variables and methods
+        String userName; //camelCase
+//        PascalCase
+//        used for classes and interface
+    }
+}
---
+++ src/comments_and_variables/L01.java
@@ -0,0 +1,22 @@
+package comments_and_variables;
+
+public class L01 {
+    static void main(String[] args) {
+        // below code is for creating user
+//        System.out.println("Hello World");
+        System.out.println("Namaste Duniya");
+        int age; // declaration
+        age = 19; // initialization
+        age = 20; // update
+        String fname = "Rahul";
+        String lname = "Goyal";
+        String name = fname + " " + lname;
+        System.out.println(name + " is "+age+" years old");
+//        string + string = string Concatination
+//        string + int = string Concatination
+//        int + int = int arithmetic addition
+        int a = 10 , b = 15;
+        int sum = a+b;
+        System.out.println("Sum of "+a+" and "+b+" is "+sum);
+    }
+}
