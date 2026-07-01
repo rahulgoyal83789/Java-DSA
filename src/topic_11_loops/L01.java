df56e25d43aeddc1a15831eea69f3f997cb34164
Rahul Goyal <rahulgoyal83789@gmail.com>
2026-07-01 21:49:02 +0530

    Loops started

---
+++ src/topic_11_loops/L01.java
@@ -0,0 +1,9 @@
+package topic_11_loops;
+
+public class L01 {
+    static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello World");
+        }
+    }
+}
