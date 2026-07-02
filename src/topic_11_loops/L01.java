ad799fe3df4af1d9f02db947fa797e44206271d6
Rahul Goyal <rahulgoyal83789@gmail.com>
2026-07-02 21:31:56 +0530

    Loops continued and resources added

--- src/topic_11_loops/L01.java
+++ src/topic_11_loops/L01.java
@@ -5,5 +5,10 @@ public class L01 {
         for (int i = 0; i < 5; i++) {
             System.out.println("Hello World");
         }
+        int a = 0;
+        while (a<5) {
+            System.out.println("Hello World");
+            a++;
+        }
     }
 }
