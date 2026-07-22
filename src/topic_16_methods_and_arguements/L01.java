e958251147e13d1cbb22c8da969985d9d4f9625e
Rahul Goyal <rahulgoyal83789@gmail.com>
2026-07-22 21:15:41 +0530

    practice

--- src/topic_16_methods_and_arguements/L01.java
+++ src/topic_16_methods_and_arguements/L01.java
@@ -4,6 +4,16 @@ public class L01 {
     static void systemCheck() {
         System.out.println("System check");
     }
+    static void swapping(int[] arr) {
+        int start = 0, end = arr.length-1;
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
     static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello World");
